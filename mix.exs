ef65bc1e21e94bc8456af971a3d9f0fd303781f7
Derek Zhou <derek@3qin.us>
2023-09-09 17:39:55 +0000

    update version and translation

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gara.MixProject do
   def project do
     [
       app: :gara,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers() ++ [:surface],
